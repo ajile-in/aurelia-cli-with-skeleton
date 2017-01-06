b7ebb1760f50fae8a4c74b89941f1d8f4b33f2a6
Ajay Jadhav <ajay@ayanworks.com>
2017-01-06 18:21:53 +0530

    Cleanup

--- server/app.js
+++ server/app.js
@@ -1,34 +1,41 @@
 'use strict';
 
-// simple express server
+// Simple express server
 var express = require('express');
 var path = require('path');
 var app = express();
-var router = express.Router();
 
 const appRoot = path.normalize(path.join(__dirname, '/..'));
 console.log('appRoot : ' + appRoot);
 
+//Allow CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+//Refer bundled aurelia scripts
 app.use("/scripts", express.static(path.join(appRoot, 'scripts')));
+
+//Start Aurelia's index.html
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: appRoot });
 });
 
-app.get('/users', function(req, res) {
-    var users = [
+//APIs
+app.get('/api', function(req, res) {
+    res.send("<h1>Welcome to Express API.</h1>");
+});
+
+app.get('/api/users', function(req, res) {
+    let users = [
         { 'Id': 1, 'Name': 'Ajay Jadhav', 'Email': 'jadhavajay@gmail.com' },
         { 'Id': 2, 'Name': 'Vijay Chavan', 'Email': 'vijay@gmail.com' },
         { 'Id': 3, 'Name': 'Sanjay Jha', 'Email': 'sanjay@gmail.com' },
         { 'Id': 4, 'Name': 'Rajan Sharma', 'Email': 'rajan@gmail.com' },
     ];
 
-    //console.log("users: " + JSON.stringify(users));
     res.send(users);
 });
 
