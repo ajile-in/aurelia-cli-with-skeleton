38be0059a0b3e7889e4236a1642a2c3bd371f245
Ajay Jadhav <ajay@ayanworks.com>
2017-01-06 16:52:55 +0530

    API Call testing

--- server/app.js
+++ server/app.js
@@ -9,6 +9,12 @@ var router = express.Router();
 const appRoot = path.normalize(path.join(__dirname, '/..'));
 console.log('appRoot : ' + appRoot);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/scripts", express.static(path.join(appRoot, 'scripts')));
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: appRoot });
@@ -26,7 +32,8 @@ app.get('/users', function(req, res) {
         { 'Id': 3, 'Name': 'Sanjay Jadhav', 'Email': 'jadhavajay@gmail.com' },
         { 'Id': 4, 'Name': 'Rajan Jadhav', 'Email': 'jadhavajay@gmail.com' },
     ];
-    console.log("users: }" + users);
+
+    console.log("users: " + JSON.stringify(users));
     res.send(users);
 });
 
--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
 import { inject } from 'aurelia-framework';
-import { HttpClient } from 'aurelia-fetch-client';
+import { HttpClient, json } from 'aurelia-fetch-client';
 
 @inject(HttpClient)
 export class App {
-    constructor(http) {
-        this.message = 'Hello World! - Users';
+  constructor(http) {
+    this.message = 'Hello World! - Users';
+    this.http = http;
+    this.users = [];
 
-        this.http = http;
+    this.http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl('http://localhost:3000/');
+    });
+  }
 
-        this.users = [];
+  activate() {
+    // ensure fetch is polyfilled before we create the http client
+    try {
+      //await fetch;
+      const response = this.http.fetch('users')
+        .then(response => response.json())
+        .then(users => {
+          this.users = users;
+          console.log("***** SUCCESS : ");
+        });
+
+    } catch (err) {
+      console.log("***** Error : " + err);
     }
-
-    async activate() {
-        // ensure fetch is polyfilled before we create the http client
-        try {
-            await fetch;
-            const http = this.http = this.getHttpClient();
-
-            http.configure(config => {
-                config
-                    .useStandardConfiguration()
-                    .withBaseUrl('http://localhost:3000/');
-            });
-
-            const response = await http.fetch('users');
-            this.users = await response.json();
-        } catch (err) {
-            console.log("***** Error : " + err);
-        }
-    }
+  }
 }
